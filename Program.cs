ef91418a7cb39b3f712bbf654c5a03179edd7b41
feherszabolcs <feherszabi2005@gmail.com>
2022-12-06 20:25:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace sqlScript2
+{
+    public class Program
+    {
+         static string path = "borvidekek.txt"; // érdemes "copy always" módon bemásolni az output könyvtárba!
+         static string dbName = "borvidek"; // a létrehozott tábla neve
+        //public static int propCount = 0;
+        static void Main(string[] args)
+        {
+            //Console.Write("Jellemzők (oszlopok) száma: ");
+
+            if (path == "") Console.WriteLine("először állíts be elérési utat a txt-hez!");
+            else Insert(path);
+        }
+        static void Insert(string path) 
+        {
+            List<Table> data = new List<Table>();
+            List<Table> firstLine = new List<Table>();           
+            foreach (var item in File.ReadAllLines(path).Skip(1))
+            {
+                data.Add(new Table(item));
+            }
+            firstLine.Add(new Table(File.ReadAllLines(path)[0]));
+
+
+
+            List<string> sql = new List<string>();
+            sql.Add($"INSERT INTO {dbName} VALUES");
+            foreach (var item in data)
+            {
+                if(int.Parse(item.id) != data.Count)
+                {
+                    sql.Add($"({item.id}, '{item.prop1}', '{item.prop2}'),");
+                }
+                else sql.Add($"({item.id}, '{item.prop1}', '{item.prop2}');"); //utolsó insert
+            }
+            File.WriteAllLines($"{dbName}.sql", sql);
+        }
+    }
+}
---
+++ Table.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sqlScript2
+{
+    public class Table
+    {
+        public string id { get; private set; }
+        public string prop1 { get; private set; }
+        public string prop2 { get; private set; }
+
+
+        //public string prop3 { get; private set; }
+        //public string prop4 { get; private set; }
+        //public string prop5 { get; private set; }
+
+        
+         
+        public Table(string line)
+        {
+            string[] m = line.Split(';');
+            if(m.Length == 2)
+            {
+                id = m[0];
+                prop1 = m[1];
+                prop2 = m[2];
+                //prop3 = m[3];
+                //prop4 = m[4];
+                //prop5 = m[5];
+            }
+            else Console.WriteLine("Módosítani kell a jellemzők számát! (Table.cs)");
+        }
+        //Több jellemmzőnél csak töröld ki a kommenteket
+    }
+}
