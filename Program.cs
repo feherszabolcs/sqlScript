80e0631d6fd2f0db30c6e08c4ec5c1662e8de7b5
feherszabolcs <feherszabi2005@gmail.com>
2022-12-12 12:47:37 +0100

    saving location added, cols up to 10

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace sqlScript2
         [STAThread]
         static void Main(string[] args)
         {
-            Console.Write("Adja meg a létrehozandó tábla nevét: "); //TODO: hiba ellenőrzés, regex
+            Console.Write("Adja meg a létrehozandó tábla nevét: ");
             dbName = Console.ReadLine();
 
             Console.WriteLine("Válassza ki a txt fájl elérési útját!");
@@ -22,7 +22,7 @@ namespace sqlScript2
 
 
 
-            if (path == "" || dbName == "") Console.WriteLine("először állíts be elérési utat a txt-hez és táblanevet! (Program.cs 10-11. sor )");
+            if (path == "" || dbName == "") Console.WriteLine("először állíts be táblanevet!");
             else Insert(path);
         }
         static int GetPropCount(string path)
@@ -47,7 +47,25 @@ namespace sqlScript2
             List<Table> data = new List<Table>();
             string[] m;
             List<string> sql = new List<string>();
-            sql.Add($"INSERT INTO {dbName} VALUES");
+            firstLineInsert(sql);
+            if (propCount == 2) //2prop
+            {
+
+                foreach (var item in File.ReadAllLines(path).Skip(1))
+                {
+                    m = item.Split(';');
+                    data.Add(new Table(m[0], m[1]));
+                }
+
+                foreach (var item in data)
+                {
+                    if (item.ID != data.Last().ID)
+                    {
+                        sql.Add($"({item.ID}, '{item.P1}'),");
+                    }
+                    else sql.Add($"({item.ID}, '{item.P1}');");
+                }
+            }
             if (propCount == 3) //3prop
             {
                
@@ -65,7 +83,6 @@ namespace sqlScript2
                     }
                     else sql.Add($"({item.ID}, '{item.P1}', '{item.P2}');"); //utolsó insert
                 }
-                File.WriteAllLines($"{dbName}.sql", sql);
             }
             if (propCount == 4) //4prop
             {
@@ -83,7 +100,6 @@ namespace sqlScript2
                     }
                     else sql.Add($"({item.ID}, '{item.P1}', '{item.P2}', '{item.P3}');");
                 }
-                File.WriteAllLines($"{dbName}.sql", sql);
             }
             if (propCount == 5) //5prop
             {
@@ -101,13 +117,122 @@ namespace sqlScript2
                     }
                     else sql.Add($"({item.ID}, '{item.P1}', '{item.P2}', '{item.P3}', '{item.P4}');");
                 }
-                File.WriteAllLines($"{dbName}.sql", sql);
             }
+            if (propCount == 6) //6prop
+            {
+                foreach (var item in File.ReadAllLines(path).Skip(1))
+                {
+                    m = item.Split(';');
+                    data.Add(new Table(m[0], m[1], m[2], m[3], m[4], m[5]));
+                }
 
+                foreach (var item in data)
+                {
+                    if (item.ID != data.Last().ID)
+                    {
+                        sql.Add($"({item.ID}, '{item.P1}', '{item.P2}', '{item.P3}', '{item.P4}', '{item.P5}'),");
+                    }
+                    else sql.Add($"({item.ID}, '{item.P1}', '{item.P2}', '{item.P3}', '{item.P4}', '{item.P5}');");
+                }
+            }
+            if (propCount == 7) //7prop
+            {
+                foreach (var item in File.ReadAllLines(path).Skip(1))
+                {
+                    m = item.Split(';');
+                    data.Add(new Table(m[0], m[1], m[2], m[3], m[4], m[5], m[6]));
+                }
 
+                foreach (var item in data)
+                {
+                    if (item.ID != data.Last().ID)
+                    {
+                        sql.Add($"({item.ID}, '{item.P1}', '{item.P2}', '{item.P3}', '{item.P4}', '{item.P5}', '{item.P6}'),");
+                    }
+                    else sql.Add($"({item.ID}, '{item.P1}', '{item.P2}', '{item.P3}', '{item.P4}', '{item.P5}', '{item.P6}');");
+                }
+            }
+            if (propCount == 8) //8prop
+            {
+                foreach (var item in File.ReadAllLines(path).Skip(1))
+                {
+                    m = item.Split(';');
+                    data.Add(new Table(m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7]));
+                }
 
+                foreach (var item in data)
+                {
+                    if (item.ID != data.Last().ID)
+                    {
+                        sql.Add($"({item.ID}, '{item.P1}', '{item.P2}', '{item.P3}', '{item.P4}', '{item.P5}', '{item.P6}', '{item.P7}'),");
+                    }
+                    else sql.Add($"({item.ID}, '{item.P1}', '{item.P2}', '{item.P3}', '{item.P4}', '{item.P5}', '{item.P6}', '{item.P7}');");
+                }
+            }
+            if (propCount == 9) //9prop
+            {
+                foreach (var item in File.ReadAllLines(path).Skip(1))
+                {
+                    m = item.Split(';');
+                    data.Add(new Table(m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8]));
+                }
+
+                foreach (var item in data)
+                {
+                    if (item.ID != data.Last().ID)
+                    {
+                        sql.Add($"({item.ID}, '{item.P1}', '{item.P2}', '{item.P3}', '{item.P4}', '{item.P5}', '{item.P6}', '{item.P7}', '{item.P8}'),");
+                    }
+                    else sql.Add($"({item.ID}, '{item.P1}', '{item.P2}', '{item.P3}', '{item.P4}', '{item.P5}', '{item.P6}', '{item.P7}', '{item.P8}');");
+                }
+            }
+            if (propCount == 10) //10prop
+            {
+                foreach (var item in File.ReadAllLines(path).Skip(1))
+                {
+                    m = item.Split(';');
+                    data.Add(new Table(m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8], m[9]));
+                }
+
+                foreach (var item in data)
+                {
+                    if (item.ID != data.Last().ID)
+                    {
+                        sql.Add($"({item.ID}, '{item.P1}', '{item.P2}', '{item.P3}', '{item.P4}', '{item.P5}', '{item.P6}', '{item.P7}', '{item.P8}', '{item.P9}'),");
+                    }
+                    else sql.Add($"({item.ID}, '{item.P1}', '{item.P2}', '{item.P3}', '{item.P4}', '{item.P5}', '{item.P6}', '{item.P7}', '{item.P8}', '{item.P9}');");
+                }
+            }
+            Saving(sql);
 
             
+        }
+        static void Saving(List<string> sql) // fájl mentési helye 
+        {
+            SaveFileDialog sv = new SaveFileDialog();
+            string path = "";
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            if (fb.ShowDialog() == DialogResult.OK)
+            {
+                Console.WriteLine(fb.SelectedPath);
+                path = fb.SelectedPath;
+            }
+
+            sv.InitialDirectory = path;
+            sv.RestoreDirectory = true;
+
+            File.WriteAllLines($"{sv.InitialDirectory}/{dbName}.sql", sql);
+        }
+        static void firstLineInsert(List<string> sql)
+        {
+            string[] fLine = File.ReadAllLines(path)[0].Split(';');
+            string pattern = "";
+            foreach (var item in fLine)
+            {
+                if(item == fLine.Last()) pattern += $"{item}";
+                else pattern += $"{item},";
+            }
+            sql.Add($"INSERT INTO {dbName}({pattern}) VALUES");
         }
         
     }
--- Table.cs
+++ Table.cs
@@ -18,10 +18,14 @@ namespace sqlScript2
         public string P7 { get; private set; }
         public string P8 { get; private set; }
         public string P9 { get; private set; }
-        //TODO up to 10
 
 
 
+        public Table(string id, string prop1)
+        {
+            ID = id;
+            P1 = prop1;
+        }
         public Table(string id,string prop1, string prop2) 
         {
            ID = id;
@@ -51,6 +55,52 @@ namespace sqlScript2
             P3 = prop3;
             P4 = prop4;
             P5 = prop5;
+        }
+        public Table(string id, string prop1, string prop2, string prop3, string prop4, string prop5, string prop6)
+        {
+            ID = id;
+            P1 = prop1;
+            P2 = prop2;
+            P3 = prop3;
+            P4 = prop4;
+            P5 = prop5;
+            P6 = prop6;
+        }
+        public Table(string id, string prop1, string prop2, string prop3, string prop4, string prop5, string prop6, string prop7)
+        {
+            ID = id;
+            P1 = prop1;
+            P2 = prop2;
+            P3 = prop3;
+            P4 = prop4;
+            P5 = prop5;
+            P6 = prop6;
+            P7 = prop7;
+        }
+        public Table(string id, string prop1, string prop2, string prop3, string prop4, string prop5, string prop6, string prop7, string prop8)
+        {
+            ID = id;
+            P1 = prop1;
+            P2 = prop2;
+            P3 = prop3;
+            P4 = prop4;
+            P5 = prop5;
+            P6 = prop6;
+            P7 = prop7;
+            P8 = prop8;
+        }
+        public Table(string id, string prop1, string prop2, string prop3, string prop4, string prop5, string prop6, string prop7, string prop8, string prop9)
+        {
+            ID = id;
+            P1 = prop1;
+            P2 = prop2;
+            P3 = prop3;
+            P4 = prop4;
+            P5 = prop5;
+            P6 = prop6;
+            P7 = prop7;
+            P8 = prop8;
+            P9 = prop9;
         }
     }
 }
